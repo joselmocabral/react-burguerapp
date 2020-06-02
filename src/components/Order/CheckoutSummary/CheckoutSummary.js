186b4f6b20fa0d9dd20756295a647d1d3d7efa10
Joselmo Cabral <joselmocabral@Joselmos-MacBook-Air.local>
2020-06-02 18:22:23 -0300

    Created Input Elements and Connected to the ContactData

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -8,7 +8,7 @@ const checkoutSummary = (props) => {
   return (
     <div className={classes.CheckoutSummary}>
       <h1>We hope it tastes well!</h1>
-      <div style={{ width: '100%', margin: 'auto' }}>
+      <div style={{ width: '100%', margin: 'auto', alignItems: 'center' }}>
         <Burger ingredients={props.ingredients} />
       </div>
       <Button clicked={props.checkoutCanceled} btnType="Danger">
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,15 +4,63 @@ import axios from '../../../axios-orders';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: '',
+    orderForm: {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Name',
+        },
+        value: '',
+      },
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Street',
+        },
+        value: '',
+      },
+      zipCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'ZIP Code',
+        },
+        value: '',
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Country',
+        },
+        value: '',
+      },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Your E-Mail',
+        },
+        value: '',
+      },
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            { value: 'fastest', displayValue: 'Fastest' },
+            { value: 'cheapest', displayValue: 'Cheapest' },
+          ],
+        },
+        value: '',
+      },
     },
+
     loading: false,
   };
 
@@ -23,16 +71,6 @@ class ContactData extends Component {
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.totalPrice, //In a production enviroment this should be calculated on the server
-      customer: {
-        name: 'Joselmo Cabral',
-        address: {
-          street: 'Teststreet 1',
-          zipCode: '213133323',
-          country: 'Brazil',
-        },
-        email: 'test@test.com',
-      },
-      deliveryMethod: 'fastest',
     };
     axios
       .post('/orders.json', order)
@@ -55,26 +93,25 @@ class ContactData extends Component {
           <Spinner />
         ) : (
           <form>
-            <input
-              className={classes.Input}
-              type="text"
-              name="name"
-              placeholder="Your Name"
+            <Input
+              elementType="input"
+              elementConfig="text"
+              value="name"
             />
-            <input
-              className={classes.Input}
+            <Input
+              inputtype="input"
               type="text"
               name="email"
               placeholder="Your email"
             />
-            <input
-              className={classes.Input}
+            <Input
+              inputtype="input"
               type="text"
               name="street"
               placeholder="Your Street"
             />
-            <input
-              className={classes.Input}
+            <Input
+              inputtype="input"
               type="text"
               name="postal"
               placeholder="Your Postal"
