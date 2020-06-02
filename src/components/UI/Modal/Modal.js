a769b723d46dc9b2d99e46f8f2d7e372a43234a9
Joselmo Cabral <joselmocabral@Joselmos-MacBook-Air.local>
2020-06-01 23:28:51 -0300

    Fetching orders from the server and error handler

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -5,9 +5,12 @@ import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
-//This could be a functional component, this is just a tiny optimization
+  //This could be a functional component, this is just a tiny optimization
   shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
+    return (
+      nextProps.show !== this.props.show ||
+      nextProps.children !== this.props.children
+    );
   }
 
   render() {
